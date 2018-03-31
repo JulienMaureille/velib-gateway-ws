33a38851cd23835de9f08f29875776a40fffa92e
Julien Maureille <julien.maureille@etu.unice.fr>
2018-03-31 22:47:30 +0200

    init

---
+++ IWS/IService1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace iws
+{
+    [ServiceContract]
+    public interface IService1
+    {
+        /**
+         * Renvoit une liste contenant les informations en temps réel de la station et dans la ville passée en paramètre
+         **/
+        [OperationContract]
+        List<string> GetInfo(string station, string cityName);
+
+        [OperationContract]
+        List<string> GetStations(string cityName);
+
+        [OperationContract]
+        List<string> GetCities();
+    }
+}
